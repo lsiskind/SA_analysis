1fba1c18c810baca5ad16a8836a5b3b781647c58
lsiskind <84937499+lsiskind@users.noreply.github.com>
2021-06-03 18:41:53 -0400

    Add files via upload

--- SA_analysis.m
+++ SA_analysis.m
@@ -4,32 +4,28 @@
 close all
 clear
 clc
+tic
 
 % read in CSV
 M = readtable('SA_data_reduced_60x.csv');
 
-% seperate time stamp from angle data
+% import timestamp
 timestamp = M(:,1);
 timestamp = table2array(timestamp);
 
 % attitude data
-posX = table2array(M(:,4)); % [miles ??]
+posX = table2array(M(:,4)); % [km]
 posY = table2array(M(:,5));
 posZ = table2array(M(:,6));
 
-% define SARJs
-SSARJ = table2array(M(:,7));
+% define SARJs (solar alpha rotary joints)
+SSARJ = table2array(M(:,7)); % [deg]
 PSARJ = table2array(M(:,8));
 
-% define beta angles per array
-P44A_BGA = table2array(M(:,9));
+% define bgas per array (beta gimbly assembly)
+P44A_BGA = table2array(M(:,9)); % [deg]
 P42A_BGA = table2array(M(:,10));
-P42B_BGA = table2array(M(:,11));
-P64B_BGA = table2array(M(:,12));
-S43A_BGA = table2array(M(:,13));
-S41A_BGA = table2array(M(:,14));
-S61B_BGA = table2array(M(:,15));
-S63B_BGA = table2array(M(:,16));
+
 
 %% convert time format
 
@@ -39,77 +35,114 @@ DateTime = datetime(timestamp,'InputFormat','uuuu:DDD:HH:mm:ss');
 % convert date-time to number of days since Jan 0, 0000
 DateNum = datenum(DateTime);
 
-%start time at 0
+%start time at 0 days, 0 seconds
 timevec = DateNum - DateNum(1).*ones(size(DateNum));
 
+% convert date-time to utc
+[year, month, day, hour, mins, sec] = datevec(DateTime);
+utc = datevec(DateTime);
+
+%% convert position to lat/long
+
+r = [posX posY posZ];
+lla = eci2lla(r, utc);
+
+lat = lla(:,1);
+long = lla(:,2);
+h = lla(:,3);
+
+%% Calculate orbit beta angle
+
+% ecliptic true solar longitude
+GMTOffset = 0;
+Gamma = solar_ecliptic(year, month, day, hour, mins, GMTOffset);
+
+% calculate RAAN
+% need velocity
+
 %% interpolate
-% might work when we can start filtering out data
-% 
-% % decrease step size to every second
+
+% % decrease step size to every 10 second
 % [rows, ~] = size(timevec);
 % timevec_int = timevec(1):1/(rows*6):timevec(end); % step size of 10 seconds
+% timevec_int = timevec_int';
 % 
 % % interpolate PSARJ and BGA angles for 2A and 4A
 % PSARJ_int = interp1(timevec, PSARJ, timevec_int);
 % P44A_BGA_int = interp1(timevec,P44A_BGA,timevec_int);
 % P42A_BGA_int = interp1(timevec,P42A_BGA,timevec_int);
+% lat_int = interp1(timevec, lat, timevec_int);
+
+%% filter out >|52| lat
+
+timevec_flt = timevec(lat<=abs(52));
+PSARJ_flt = PSARJ(lat<=abs(52));
+P44A_BGA_flt = P44A_BGA(lat<=abs(52));
+P42A_BGA_flt = P42A_BGA(lat<=abs(52));
+
+% repeat with interpolated data
+% timevec_int = timevec_int(lat_int<=abs(52));
+% PSARJ_int = PSARJ_int(lat_int<=abs(52));
+% P44A_BGA_int = P44A_BGA_int(lat_int<=abs(52));
+% P42A_BGA_int = P42A_BGA_int(lat_int<=abs(52));
 
 %% find intrusions into kov and fov
 
 % time instances and alpha angles of intrusions into EMIT FOV
-t2A_fov = timevec(PSARJ >= 268 & PSARJ <= 276);
-PSARJ_2Afov = PSARJ(PSARJ >= 268 & PSARJ <= 276);
+t2A_fov = timevec_flt(PSARJ_flt >= 268 & PSARJ_flt <= 276);
+PSARJ_2Afov = PSARJ_flt(PSARJ_flt >= 268 & PSARJ_flt <= 276);
 
-t4A_fov = timevec(PSARJ >= 89 & PSARJ <= 96);
-PSARJ_4Afov = PSARJ(PSARJ >= 89 & PSARJ <= 96);
+t4A_fov = timevec_flt(PSARJ_flt >= 89 & PSARJ_flt <= 96);
+PSARJ_4Afov = PSARJ_flt(PSARJ_flt >= 89 & PSARJ_flt <= 96);
 
 % time instances and alpha angles of intrusions into stray light KOZ envelope
-t2A_koz_a = timevec(PSARJ>=255 & PSARJ<=289);
-PSARJ_2Akoz = PSARJ(PSARJ>=255 & PSARJ<=289);
+t2A_koz_a = timevec_flt(PSARJ_flt>=255 & PSARJ_flt<=289);
+PSARJ_2Akoz = PSARJ_flt(PSARJ_flt>=255 & PSARJ_flt<=289);
 
-t4A_koz_a = timevec(PSARJ>=75 & PSARJ<=110); 
-PSARJ_4Akoz = PSARJ(PSARJ>=75 & PSARJ<=110);
+t4A_koz_a = timevec_flt(PSARJ_flt>=75 & PSARJ_flt<=110); 
+PSARJ_4Akoz = PSARJ_flt(PSARJ_flt>=75 & PSARJ_flt<=110);
 
-% time instances and beta angles of intrusions into KOZ
-t2A_koz_b = timevec((PSARJ == 270 & (P42A_BGA >=24 & P42A_BGA <= 150)) | (PSARJ == 270 & (P42A_BGA >=229 & P42A_BGA <= 337)));
-BGA_2Akoz = P42A_BGA((PSARJ == 270 & (P42A_BGA >=24 & P42A_BGA <= 150)) | (PSARJ == 270 & (P42A_BGA >=229 & P42A_BGA <= 337)));
+% time instances and beta angles of intrusions into KOZ (tolerances??)
+t2A_koz_b = timevec_flt(((PSARJ_flt>=269.99 & PSARJ_flt <= 270.01) & (P42A_BGA_flt >=24 & P42A_BGA_flt <= 150)) | ((PSARJ_flt>=269.99 & PSARJ_flt <= 270.01)& (P42A_BGA_flt >=229 & P42A_BGA_flt <= 337)));
+BGA_2Akoz = P42A_BGA_flt(((PSARJ_flt>=269.99 & PSARJ_flt <= 270.01) & (P42A_BGA_flt >=24 & P42A_BGA_flt <= 150)) | ((PSARJ_flt>=269.99 & PSARJ_flt <= 270.01) & (P42A_BGA_flt >=229 & P42A_BGA_flt <= 337)));
 
-t4A_koz_b = timevec((PSARJ == 90 & (P44A_BGA >=41 & P44A_BGA<=149)) | (PSARJ == 90 & (P44A_BGA>=230 & P44A_BGA<=338)));
-BGA_4Akoz = P44A_BGA((PSARJ == 90 & (P44A_BGA >=41 & P44A_BGA<=149)) | (PSARJ == 90 & (P44A_BGA>=230 & P44A_BGA<=338)));
+t4A_koz_b = timevec_flt(((PSARJ_flt>=89.99 & PSARJ_flt <= 90.01) & (P44A_BGA_flt >=41 & P44A_BGA_flt<=149)) | ((PSARJ_flt>=89.99 & PSARJ_flt <= 90.01) & (P44A_BGA_flt>=230 & P44A_BGA_flt<=338)));
+BGA_4Akoz = P44A_BGA_flt(((PSARJ_flt>=89.99 & PSARJ_flt <= 90.01) & (P44A_BGA_flt >=41 & P44A_BGA_flt<=149)) | ((PSARJ_flt>=89.99 & PSARJ_flt <= 90.01) & (P44A_BGA_flt>=230 & P44A_BGA_flt<=338)));
 
-% % repeat beta angles with interpolated data
-% t2A_koz_b_int = timevec_int((PSARJ_int == 270 & (P42A_BGA_int >=24 & P42A_BGA_int <= 150)) | (PSARJ_int == 270 & (P42A_BGA_int >=229 & P42A_BGA_int <= 337)));
-% BGA_2Akoz_int = P42A_BGA_int((PSARJ_int == 270 & (P42A_BGA_int >=24 & P42A_BGA_int <= 150)) | (PSARJ_int == 270 & (P42A_BGA_int >=229 & P42A_BGA_int <= 337)));
+% repeat beta angles with interpolated data (tolerances???)
+% t2A_koz_b_int = timevec_int(((PSARJ_int>=269.99 & PSARJ_int <= 270.01) & (P42A_BGA_int >=24 & P42A_BGA_int <= 150)) | ((PSARJ_int>=269.99 & PSARJ_int <= 270.01) & (P42A_BGA_int >=229 & P42A_BGA_int <= 337)));
+% BGA_2Akoz_int = P42A_BGA_int((P(PSARJ_int>=269.99 & PSARJ_int <= 270.01)& (P42A_BGA_int >=24 & P42A_BGA_int <= 150)) | ((PSARJ_int>=269.99 & PSARJ_int <= 270.01) & (P42A_BGA_int >=229 & P42A_BGA_int <= 337)));
 % 
-% t4A_koz_b_int = timevec_int((PSARJ_int == 90 & (P44A_BGA_int >=41 & P44A_BGA_int <= 149)) | (PSARJ_int == 90 & (P44A_BGA_int >= 230 & P44A_BGA_int <=338)));
-% BGA_4Akoz_int = P44A_BGA_int((PSARJ_int == 90 & (P44A_BGA_int >=41 & P44A_BGA_int <= 149)) | (PSARJ_int == 90 & (P44A_BGA_int >=230 & P44A_BGA_int <=338)));
+% t4A_koz_b_int = timevec_int(((PSARJ_int>=89.99 & PSARJ_int <= 90.01) & (P44A_BGA_int >=41 & P44A_BGA_int <= 149)) | ((PSARJ_int>=89.99 & PSARJ_int <= 90.01) & (P44A_BGA_int >= 230 & P44A_BGA_int <=338)));
+% BGA_4Akoz_int = P44A_BGA_int(((PSARJ_int>=89.99 & PSARJ_int <= 90.01) & (P44A_BGA_int >=41 & P44A_BGA_int <= 149)) | ((PSARJ_int>=89.99 & PSARJ_int <= 90.01) & (P44A_BGA_int >=230 & P44A_BGA_int <=338)));
 
 
 %% Percent overlap
 
-percFOV = ((sum(t2A_fov)+sum(t4A_fov))/sum(timevec))*100; % percentage of time
-percKOZ = ((sum(t2A_koz_a) + sum(t4A_koz_a))/sum(timevec))*100;
-perc4A = ((sum(t4A_fov) + sum(PSARJ_4Akoz)))/sum(timevec)*100;
+percFOV = ((sum(t2A_fov)+sum(t4A_fov))/sum(timevec_flt))*100; % percentage of time
+percKOZ = ((sum(t2A_koz_a) + sum(t4A_koz_a))/sum(timevec_flt))*100;
+
+%% total time lost
 
 
 %% make plots
 
 % plot orbit of ISS
 figure()
-Re = 3958.8; % [miles]
+Re = 6397; % [km]
 plot3(posX, posY, posZ);
 hold on
 [XS, YS, ZS] = sphere(30); % plot the Earth using Matlab sphere command
 surf(XS*Re, YS*Re, ZS*Re);
-xlabel('x [miles]');
-ylabel('y [miles]');
-zlabel('z [miles]');
+xlabel('x [km]');
+ylabel('y [km]');
+zlabel('z [km]');
 title('Orbit of ISS Around Earth');
 hold off
 
 % alpha angles
 figure()
-plot(timevec, PSARJ)
+plot(timevec_flt, PSARJ_flt)
 hold on
 plot(t2A_fov, PSARJ_2Afov);
 plot(t4A_fov, PSARJ_4Afov);
@@ -120,7 +153,7 @@ title(['ISS SAW PSARJs intrude into EMIT FOV ' num2str(percFOV) '% of 2019']);
 hold off
 
 figure()
-plot(timevec, PSARJ)
+plot(timevec_flt, PSARJ_flt)
 hold on
 plot(t2A_koz_a, PSARJ_2Akoz)
 plot(t4A_koz_a, PSARJ_4Akoz)
@@ -132,9 +165,9 @@ hold off
 
 % beta angles
 figure()
-plot(timevec, P42A_BGA);
+plot(timevec_flt, P42A_BGA_flt);
 hold on
-plot(t2A_koz_b, BGA_2Akoz);
+plot(t2A_koz_b, BGA_2Akoz,'*');
 legend('P4-2A BGA Angles','P4-2A intrusions into EMIT KOZ');
 ylabel('\beta [deg]');
 xlabel('Time [days]');
@@ -142,16 +175,16 @@ title('\alpha=270^o, Beta angles resulting in P4-2A SAW Intrusions into EMIT KOZ
 hold off
 
 figure()
-plot(timevec, P44A_BGA);
+plot(timevec_flt, P44A_BGA_flt);
 hold on
-plot(t4A_koz_b, BGA_4Akoz);
+plot(t4A_koz_b, BGA_4Akoz,'*');
 legend('P4-4A BGA Angles','P4-4A intrusions into EMIT KOZ');
 ylabel('\beta [deg]');
 xlabel('Time [days]');
 title('\alpha=90^o, Beta angles resulting in P4-4A SAW Intrusions into EMIT KOZ');
 hold off
 
-% beta anlges with interpolated data
+% % beta anlges with interpolated data
 % figure()
 % plot(timevec_int, P42A_BGA_int);
 % hold on
@@ -172,4 +205,29 @@ hold off
 % title({'\alpha=90^o, Beta angles resulting in P4-4A SAW Intrusions into EMIT KOZ'},{'Interpolated'});
 % hold off
 
+% histograms
 
+figure() % PSARJ into FOV
+histogram(PSARJ_flt);
+hold on
+histogram(PSARJ_2Afov);
+histogram(PSARJ_4Afov);
+legend('PSARJ angles','P4-2A Intrusion (268^o - 276^o)','P4-4A Intrusion (89^o - 96^o)','Location','northwest')
+xlabel('Angles [deg]');
+ylabel('Frequency');
+title(['ISS SAW PSARJs intrude into EMIT FOV ' num2str(percFOV) '% of 2019']);
+hold off
+
+figure() % PSARJ into KOZ
+histogram(PSARJ_flt);
+hold on
+histogram(PSARJ_2Akoz);
+histogram(PSARJ_4Akoz);
+legend('PSARJ angles','P4-2A Intrusion (255^o - 289^o)','P4-4A Intrusion (75^o - 110^o)','Location','northwest')
+xlabel('Angles [deg]');
+ylabel('Frequency');
+title(['ISS SAW PSARJs intrude into EMIT KOZ ' num2str(percKOZ) '% of 2019']);
+hold off
+
+
+toc
--- SA_analysis_full.m
+++ SA_analysis_full.m
@@ -9,36 +9,27 @@ clc
 % import from two-week and year-long data set
 
 % read in CSV from both data sets
-M2 = readtable('iss_bad_data_joined_365.csv'); % 2 weeks
-M = readtable('SA_data_reduced_60x.csv'); % 1 year every minute
+M = readtable('SA_data_reduced_60x.csv'); % every minute for 1 year
+M2 = readtable('iss_bad_data_joined_365.csv'); % every sec for 2 weeks
 
 % position data (ECI frame)
-posX2 = table2array(M2(:,5)); % [km]
-posY2 = table2array(M2(:,6));
-posZ2 = table2array(M2(:,7));
-
 posX = table2array(M(:,4)); % [km]
 posY = table2array(M(:,5));
 posZ = table2array(M(:,6));
 
-% define SARJs
+posX2 = table2array(M2(:,5)); % [km]
+posY2 = table2array(M2(:,6));
+posZ2 = table2array(M2(:,7));
+
+% define SARJs (solar alpha rotary joint)
+SSARJ = table2array(M(:,7)); % [deg] 
+PSARJ = table2array(M(:,8)); % port
+
 SSARJ2 = table2array(M2(:,8)); % [deg]
 PSARJ2 = table2array(M2(:,9));
 
-SSARJ = table2array(M(:,7));
-PSARJ = table2array(M(:,8));
-
-% define beta angles per array
-P44A_BG_2 = table2array(M2(:,10)); % [deg]
-P42A_BGA_2 = table2array(M2(:,11));
-P42B_BGA_2 = table2array(M2(:,12));
-P64B_BGA_2 = table2array(M2(:,13));
-S43A_BGA_2 = table2array(M2(:,14));
-S41A_BGA_2 = table2array(M2(:,15));
-S61B_BGA_2 = table2array(M2(:,16));
-S63B_BGA_2 = table2array(M2(:,17));
-
-P44A_BGA = table2array(M(:,9));
+% define BGAs angles per array (beta gimbal assembly)
+P44A_BGA = table2array(M(:,9)); % [deg]
 P42A_BGA = table2array(M(:,10));
 P42B_BGA = table2array(M(:,11));
 P64B_BGA = table2array(M(:,12));
@@ -47,25 +38,63 @@ S41A_BGA = table2array(M(:,14));
 S61B_BGA = table2array(M(:,15));
 S63B_BGA = table2array(M(:,16));
 
-%% convert timestamps
-% grab time stamps
-timestamp2 = M2(:,2);
-timestamp2 = table2array(timestamp2);
+P44A_BGA_2 = table2array(M2(:,10)); % [deg]
+P42A_BGA_2 = table2array(M2(:,11));
+P42B_BGA_2 = table2array(M2(:,12));
+P64B_BGA_2 = table2array(M2(:,13));
+S43A_BGA_2 = table2array(M2(:,14));
+S41A_BGA_2 = table2array(M2(:,15));
+S61B_BGA_2 = table2array(M2(:,16));
+S63B_BGA_2 = table2array(M2(:,17));
 
+%% convert timestamps
+
+% grab time stamps
 timestamp = M(:,1);
 timestamp = table2array(timestamp);
 
+timestamp2 = M2(:,2);
+timestamp2 = table2array(timestamp2);
+
 % convert timestamps to date-time format
-DateTime2 = datetime(timestamp2,'InputFormat','uuuu:DDD:HH:mm:ss');
 DateTime = datetime(timestamp,'InputFormat','uuuu:DDD:HH:mm:ss');
+DateTime2 = datetime(timestamp2,'InputFormat','uuuu:DDD:HH:mm:ss');
 
 % convert date-time to number of days since Jan 0, 0000
-DateNum2 = datenum(DateTime2);
 DateNum = datenum(DateTime);
+DateNum2 = datenum(DateTime2);
 
-% set start time at 0 seconds
-timevec2 = DateNum2 - DateNum2(1).*ones(size(DateNum2));
+% set start time to 0 days, 0 seconds
 timevec = DateNum - DateNum(1).*ones(size(DateNum));
+timevec2 = DateNum2 - DateNum2(1).*ones(size(DateNum2));
+
+%% Convert position to lat/long
+
+% 365 day data
+r = [posX posY posZ];
+lla = eci2lla(r, utc);
+
+lat = lla(:,1);
+long = lla(:,2);
+h = lla(:,3);
+
+% two week data
+r2 = [posX posY posZ];
+lla2 = eci2lla(r, utc);
+
+lat2 = lla(:,1);
+long2 = lla(:,2);
+h2 = lla(:,3);
+
+%% interpolate data
+
+
+
+
+
+
+
+
 
 %% interpolate data sets
 % try interpolation over the first two weeks. 
@@ -87,16 +116,22 @@ t4A_fov_2 = timevec2(PSARJ2 >= 89 & PSARJ2 <= 96);
 PSARJ_4Afov_2 = PSARJ2(PSARJ2 >= 89 & PSARJ2 <= 96);
 
 % time instances and alpha angles of intrusions into stray light KOZ envelope
-t2A_koz_2 = timevec2((PSARJ2>=255 & PSARJ2<=289) | (PSARJ2 == 270 & (P42A_BGA_2 >=24 & P42A_BGA_2 <= 150)) | (PSARJ2 == 270 & (P42A_BGA_2 >=229 & P42A_BGA_2 <= 337))); % FIX
-PSARJ_2Akoz_2 = PSARJ2((PSARJ2>=255 & PSARJ2<=289) | (PSARJ2 == 270 & P42A_BGA_2 >=24 & P42A_BGA_2 <= 150) | (PSARJ2 == 270 & (P42A_BGA_2 >=229 & P42A_BGA_2 <= 337)));
+t2A_koz_2 = timevec2(PSARJ2>=255 & PSARJ2<=289); 
+PSARJ_2Akoz_2 = PSARJ2(PSARJ2>=255 & PSARJ2<=289); 
 
-t4A_koz_2 = timevec2((PSARJ2>=75 & PSARJ2<=110) | (PSARJ2 == 90 & P44A_BG_2 >=41 & P44A_BG_2<=150) | (PSARJ2 == 90 & P44A_BG_2>=229 & P44A_BG_2<=337));
-PSARJ_4Akoz_2 = PSARJ2((PSARJ2>=75 & PSARJ2<=110) |(PSARJ2 == 90 & P44A_BG_2 >=41 & P44A_BG_2<=150) | (PSARJ2 == 90 & P44A_BG_2>=229 & P44A_BG_2<=337));
+t4A_koz_2 = timevec2(PSARJ2>=75 & PSARJ2<=110); 
+PSARJ_4Akoz_2 = PSARJ2(PSARJ2>=75 & PSARJ2<=110); 
+
+% time instances of beta intrusions into koz
+t2A_koz_b_2 = timevec2((PSARJ2 == 270 & (P42A_BGA_2 >=24 & P42A_BGA_2 <= 150)) | (PSARJ2 == 270 & (P42A_BGA_2 >=229 & P42A_BGA_2 <= 337)));
+BGA_2Akoz_2 = P42A_BGA_2((PSARJ2 == 270 & (P42A_BGA_2 >=24 & P42A_BGA_2 <= 150)) | (PSARJ2 == 270 & (P42A_BGA_2 >=229 & P42A_BGA_2 <= 337)));
+
+t4A_koz_b_2 = timevec2((PSARJ2 == 90 & (P44A_BGA_2 >=41 & P44A_BGA_2<=149)) | (PSARJ2 == 90 & (P44A_BGA_2>=230 & P44A_BGA_2<=338)));
+BGA_4Akoz_2 = P44A_BGA_2((PSARJ2 == 90 & (P44A_BGA_2 >=41 & P44A_BGA_2 <= 149)) | (PSARJ2 == 90 & (P44A_BGA_2 >=230 & P44A_BGA_2 <= 338)));
 
 % Percent overlap
 percFOV_2 = ((sum(t2A_fov_2)+sum(t4A_fov_2))/sum(timevec2))*100; % percentage of time
 percKOZ_2 = ((sum(t2A_koz_2) + sum(t4A_koz_2))/sum(timevec2))*100;
-perc4A_full = ((sum(t4A_fov_2) + sum(PSARJ_4Akoz_2)))/sum(timevec2)*100;
 
 % create patch for FOV intrusions
 x_2 = [timevec2(1) timevec2(end) timevec2(end) timevec2(1)];
@@ -120,7 +155,7 @@ Re = 6378; % radius of earth km
 % calculate altitude of ISS
 r = [posX2 posY2 posZ2];
 rmag = sqrt(sum(r.^2,2));
-h = rmag - Re; % h=~410 km, true to iss orbit
+alt = rmag - Re; % alt=~410 km, true to iss orbit
 
 %% Make plots
 
@@ -159,4 +194,25 @@ legend('All PSARJ Angles','P4-2A Intrusion into EMIT KOZ','P4-4A Intrusion into 
 ylabel('\alpha [deg]');
 xlabel('Time [days]');
 title(['ISS SAWs intrude into EMIT KOZ ' num2str(percKOZ_2) '% of 2019']);
+hold off
+
+% beta angles
+figure()
+plot(timevec2, P42A_BGA_2);
+hold on
+plot(t2A_koz_b_2, BGA_2Akoz_2,'*');
+legend('P4-2A BGA Angles','P4-2A intrusions into EMIT KOZ');
+ylabel('\beta [deg]');
+xlabel('Time [days]');
+title('\alpha=270^o, Beta angles resulting in P4-2A SAW Intrusions into EMIT KOZ');
+hold off
+
+figure()
+plot(timevec2, P44A_BGA_2);
+hold on
+plot(t4A_koz_b_2, BGA_4Akoz_2,'*');
+legend('P4-4A BGA Angles','P4-4A intrusions into EMIT KOZ');
+ylabel('\beta [deg]');
+xlabel('Time [days]');
+title('\alpha=90^o, Beta angles resulting in P4-4A SAW Intrusions into EMIT KOZ');
 hold off
